8305b05191ec12e621dd3f101bddac4d9315281e
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-23 10:04:44 +0200

    Generic list card

---
+++ src/components/AppListCard.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        cardObj: {
+            type: Object,
+            required: true,
+        }
+    },
+    methods: {
+        getImageUrl(imageUrl) {
+            return new URL(`../assets/flags/${imageUrl}.png`, import.meta.url).href;
+        }
+    }
+} 
+</script>
+
+<template>
+    <li>
+        <p>Titolo: {{ cardObj.title }} {{ cardObj.name }}</p>
+        <p>Titolo originale: {{ cardObj.original_title }} {{ cardObj.original_name }}</p>
+        <p class="image">Lingua: <img :src="getImageUrl(cardObj.original_language)" :alt="cardObj.original_language">
+        </p>
+        <p>Voto: {{ cardObj.vote_average }}</p>
+    </li>
+</template>
+
+<style lang="scss" scoped>
+.image {
+    background-color: gray;
+
+}
+</style>
